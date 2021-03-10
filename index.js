a86ef97a9458b4187576877683b4158d1ce77549
sumanthreddy1233 <sumanthreddy.naidu@gmail.com>
2021-03-10 01:10:04 -0600

    added dependencies of path and view engines, added .env file and added to gitignore

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 var express = require('express');
 var app = express();
 var env = require('dotenv').config()
+const path = require('path')
+app.set('./views', path.join(__dirname, 'views'))
+
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.sendFile('./views/index.html', { root: __dirname });
 });
 const MongoClient = require('mongodb').MongoClient  
 
 
 const client = new MongoClient(process.env.ATLAS_URI, { useNewUrlParser: true });
 client.connect((err, database) => {
-  console.log("--goes to db--")
+  console.log("Connected to database.")
   if (err) return console.log(err);
   var database = database.db("locator");
  app.listen(3000, function () {
