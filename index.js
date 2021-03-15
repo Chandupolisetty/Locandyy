63449dee964cc64bab17e2b70c1c7416cf0eec21
chandu polisetty <chandrakanthpolisetty@gmail.com>
2021-03-14 23:00:58 -0500

    index.ejs commit and package.json dependencies added

--- index.js
+++ index.js
@@ -2,21 +2,32 @@ var express = require('express');
 var app = express();
 var env = require('dotenv').config()
 const path = require('path')
-app.set('./views', path.join(__dirname, 'views'))
+const engines = require('consolidate')
+const bodyParser = require('body-parser');
+
+
+app.set('view engine', 'ejs')
+app.engine('ejs', engines.ejs)
+app.set('views', path.join(__dirname, './views'))
 const port = process.env.PORT || 3000
 
 app.get('/', function (req, res) {
   res.sendFile('./views/index.html', { root: __dirname });
 });
+
+
+const router = require('./routes/router')
+app.use('/', router)
+app.use(bodyParser.json({ type: "application/*+json" }));
+
+
 const MongoClient = require('mongodb').MongoClient  
-
-
 const client = new MongoClient(process.env.ATLAS_URI, { useNewUrlParser: true });
 client.connect((err, database) => {
   console.log("Connected to database.")
   if (err) return console.log(err);
   var database = database.db("locandyy");
   app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
+    console.log(`App listening at http://localhost:${port}`)
 })
 })
---
+++ routes/router.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const bodyParser = require('body-parser');
+const router = express.Router()
+router.use(bodyParser.urlencoded({ extended: true }));
+
+
+router.get('/', (req, res) => {
+    res.render('../views/index', { title: 'index' })
+})
+
+module.exports = router;
