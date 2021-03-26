cbdb283a2d68d9b20f6c05f43ed871b8ede68ac6
chandu polisetty <chandrakanthpolisetty@gmail.com>
2021-03-26 02:27:47 -0500

    fix

--- seed.js
+++ seed.js
@@ -1,7 +1,7 @@
 const  axios=require('axios');
 exports.show = (req, res) => {
     // Make a get request to /api/users
-    axios.get('http://localhost:3000/location')
+    axios.get("https:/locandyy.herokuapp.com/location")
         .then(function(response) {
             res.render('show', { location: response.data });
             // console.log("grhogiho;airhg;stihgo");
