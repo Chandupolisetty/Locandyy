cb1201d04c41294c134fe3f7d7f2a26577d330be
chandu polisetty <chandrakanthpolisetty@gmail.com>
2021-03-31 06:09:20 -0500

    added code related to speech synthesis

---
+++ public/scripts/locations.js
@@ -0,0 +1,35 @@
+export default [
+    {
+        Name: 'Colden Hall',
+        type: 'circle',
+        coordinate: [
+            {
+            latitude: 40.35108979954159, 
+            longitude: -94.88241248675814
+        }
+        ],
+
+    },
+    {
+        Name: 'Horizons West Apartments',
+        type: 'circle',
+        coordinate: [
+            {
+                latitude: 41.31447748801441,
+            longitude: -95.05660917970772
+            }
+        ]
+
+    },
+    {
+        Name: 'B.D Owens Library',
+        type: 'circle',
+        coordinate: [
+            {
+            latitude: 40.353523,
+            longitude: -94.886021
+        }
+    ]
+
+    }
+];
---
+++ public/scripts/main.js
@@ -0,0 +1,25 @@
+import locationsArray from "./locations"
+
+let colorElement = document.getElementById("field");
+let colorElement1 = document.getElementById("field1");
+
+
+window.addEventListener('load', main);
+// colorElement.addEventListener('click', onClickSquareBox2);
+// colorElement.addEventListener('touch', onClickSquareBox2);
+colorElement.addEventListener('click', onClickSquareBox1);
+colorElement.addEventListener('touch', onClickSquareBox1);
+
+function main() {
+    console.log('Page is fully loaded');
+}
+
+ function onClickSquareBox1() {
+//     document.getElementById("targetloc").innerHTML = "The Location selceted is ";
+//     document.getElementById("lbl").innerHTML = targetLoc.Name;
+//     let utterance = new SpeechSynthesisUtterance(`The location to be found is ${targetLoc.Name}`);
+//     speechSynthesis.speak(utterance);
+//    document.getElementById("device-lat").innerHTML = targetLoc.coordinates[0].latitude;
+//     document.getElementById("device-long").innerHTML = targetLoc.coordinates[1].longitude;
+console.log("---sumanth is here---")
+}
